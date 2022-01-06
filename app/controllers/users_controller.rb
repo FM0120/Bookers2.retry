6c25d1d6f1f23d331f376bec91c246f0ccd430e7
FM0120git <kaz.fumiaki@gmail.com>
2022-01-06 09:34:11 +0000

    users controller update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,42 @@
 class UsersController < ApplicationController
   def index
+    @user = current_user
+    @users = User.all
+    @book = Book.new
   end
 
   def show
+    @book = Book.find(params[:id])
+    @books = @user.book
+    @book = Book.new
   end
 
   def edit
+    @user = User.find(params[:id])
+    @book = current_user.books
+    # unless @user == current_user
+    #   redirect_to user_path(current_user.id)
+    # end
   end
+end
+
+def create
+  @user = User.new(params[:id])
+  @user.user.id = current_user.user.id
+  @user.save
+end
+
+def update
+  @user = User.find(params[:id])
+  if @user.update(user_params)
+    redirect_to user_path(@user.id), notice: "You have updated user successfully."
+  else
+    render:edit
+  end
+end
+
+private
+
+def user_params
+  params.require(:user).permit(:name, :introduction, :profile_image)
 end
