8be7b8e2c77b39a5d1e27e7b7f9800b0686fc77d
FM0120git <kaz.fumiaki@gmail.com>
2022-01-06 00:58:17 +0000

    controller 追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'books/index'
+  get 'books/show'
+  get 'books/edit'
+  get 'users/index'
+  get 'users/show'
+  get 'users/edit'
   devise_for :users
    root to: 'homes#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
