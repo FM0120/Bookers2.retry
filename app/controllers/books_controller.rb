1f6a45eb03ad040ba07a99e02555bfc0230138ff
FM0120git <kaz.fumiaki@gmail.com>
2022-01-07 08:50:08 +0000

    books controller first

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,20 @@
 class BooksController < ApplicationController
   def index
-    
+    @book = Book.all
+    @book = Book.new
+    @user = current_user
   end
 
   def show
+    @book = Book.find(prams[:id])
+    @book = Book.new
+    @user = User.find_by(id: @book.user.id)
+    redirect_to books_url if @book.blank?
+    # blank? railsの拡張。nil, "", " ", [], {} のいずれかでTrueを返す。
   end
 
   def edit
+    @book = Book.firnd(params[:id])
+    @user = @book.user
   end
 end
