5e677f31f7d6232c17361cd9defeef7fe7a6ab3d
FM0120git <kaz.fumiaki@gmail.com>
2022-01-14 02:46:39 +0000

    favorite addition

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'book_comments/destroy'
   devise_for :users
    root to: 'homes#top'
+   get '/home/about' => 'homes#about'
     get '/users/logout' => 'devise/sessions#destroy'
         resources :users,only: [:show,:index,:edit,:update, :destroy,:index]
         resources :books, only: [:new, :create, :index, :show, :edit, :destroy,:update]
