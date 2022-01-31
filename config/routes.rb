4aaf61060a0a472f4e29710d039073e20a293221
FM0120git <kaz.fumiaki@gmail.com>
2022-01-31 13:46:51 +0000

    update

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
    root to: 'homes#top'
    get '/home/about' => 'homes#about'
-    get '/users/logout' => 'devise/sessions#destroy'
-    resources :users do
-    resource :relationships, only: [:create, :destroy]
+   get '/users/logout' => 'devise/sessions#destroy'
+    
+    resources :users,only: [:show,:index,:edit,:update, :destroy,:index]  do
+        resources :relationships, only: [:create, :destroy]
         get 'followings' => 'relationships#followings', as: 'followings'
         get 'followers' => 'relationships#followers', as: 'followers'
     end
     
-    resources :users,only: [:show,:index,:edit,:update, :destroy,:index] 
     resources :books, only: [:new, :create, :index, :show, :edit, :destroy,:update] do
         resources :book_comments, only: [:create,:destroy]
         resource :favorite, only: [:create, :destroy]
